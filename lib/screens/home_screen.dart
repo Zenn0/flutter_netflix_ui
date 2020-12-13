1df7a03d0ab6138fc56106212edfa7285b308bf8
Zenno <rafa_miranda_10@hotmail.com>
2020-12-13 03:33:10 +0100

    Created the Content Header

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:netflix_ui/data/data.dart';
 import 'package:netflix_ui/widgets/widgets.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -31,6 +32,7 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     final Size screenSize = MediaQuery.of(context).size;
     return Scaffold(
+      extendBodyBehindAppBar: true,
       floatingActionButton: FloatingActionButton(
         backgroundColor: Colors.grey[800],
         child: const Icon(Icons.cast),
@@ -44,10 +46,7 @@ class _HomeScreenState extends State<HomeScreen> {
         controller: _scrollController,
         slivers: [
           SliverToBoxAdapter(
-            child: Container(
-              height: 1000.0,
-              color: Colors.blue,
-            ),
+            child: ContentHeader(featuredContent: sintelContent),
           )
         ],
       ),
