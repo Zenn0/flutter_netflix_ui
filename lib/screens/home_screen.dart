2c27475174906c6f6f307a657e0cc583963ef410
Zenno <rafa_miranda_10@hotmail.com>
2020-12-14 02:59:56 +0100

    Created the HomeScreen Content List

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -51,12 +51,30 @@ class _HomeScreenState extends State<HomeScreen> {
           SliverPadding(
             padding: const EdgeInsets.only(top: 20.0),
             sliver: SliverToBoxAdapter(
-              child: Previews(
-                title: 'Previews',
-                contentList:previews,
-                )
-            ),
-          )
+                child: Previews(
+              title: 'Previews',
+              contentList: previews,
+            )),
+          ),
+          SliverToBoxAdapter(
+              child: ContentList(
+            title: 'My List',
+            contentList: myList,
+          )),
+          SliverToBoxAdapter(
+              child: ContentList(
+            title: 'Netflix Originals',
+            contentList: originals,
+            isOriginals: true,
+          )),
+          SliverPadding(
+            padding: const EdgeInsets.only(bottom: 20.0),
+            sliver: SliverToBoxAdapter(
+                child: ContentList(
+              title: 'Trending',
+              contentList: trending,
+            )),
+          ),
         ],
       ),
     );
